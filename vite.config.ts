4f7a73abf9545e08b7154508b5d00488b97f02ba
Mark Small <mark.small@astrosat.net>
2022-09-05 22:25:01 +0100

    feat(frontend): Add library setup
    Added config/dependencies to manage a library project.

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,33 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ insertTypesEntry: true })],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/hooks/index.ts'),
+      name: 'react-utils',
+      formats: ['es', 'umd'],
+      fileName: format => `react-utils.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled.
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps.
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'),
