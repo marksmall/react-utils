5d3f9ed897a87b4a78095a979de6a747c5b78cee
Mark Small <mark.small@astrosat.net>
2022-09-21 22:21:13 +0100

    fix(frontend): Refactor to export more than just hooks
    Prior to this, the library was very specific to just the hooks. I've
    not expanded the library, so it will have many more utilities.
    
    This included re-formatting the file to match our standards, especially
    in the import ordering.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
-
 import * as path from 'path';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ insertTypesEntry: true })],
+  plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/hooks/index.ts'),
+      entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'react-utils',
       formats: ['es', 'umd'],
       fileName: format => `react-utils.${format}.js`,
@@ -28,6 +27,7 @@ export default defineConfig({
         },
       },
     },
+    sourcemap: true,
   },
   resolve: {
     alias: {
