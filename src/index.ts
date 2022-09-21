9048b5a3ba7f1ccb821e47ca6f4ee1b34085b841
Mark Small <mark.small@astrosat.net>
2022-09-21 22:28:56 +0100

    chore(frontend): Re-order exporting of components
    I moved the UI components to be the first exported as these will
    probably end up being the code most widely tinkered with, so best
    have it first.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css';
 import './index.css';
 
-export { useInterval, useTimeout, useLocalStorage } from '~/hooks';
-export { Button } from '~/components/button.component';
+export { Button } from './components/button.component';
+
+export { useInterval, useTimeout, useLocalStorage } from './hooks';
